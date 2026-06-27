61badcd2d3fc95f246e87fbd7dbce9e6af35e613
Fabricio <fepafumi97@gmail.com>
2026-06-27 04:25:57 -0300

    feat: Added Static Lists

--- app/src/main/java/com/example/tenisappf/screens/PlayersScreen.kt
+++ app/src/main/java/com/example/tenisappf/screens/PlayersScreen.kt
@@ -5,6 +5,14 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.SportsTennis
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.ListItemDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -12,6 +20,20 @@ import androidx.compose.ui.Modifier
 
 @Composable
 fun PlayersScreen(modifier: Modifier = Modifier, innerPading: PaddingValues) {
+    @Composable
+    fun PlayerListItem(title: String, subtitle: String) {
+        ListItem(
+            headlineContent = { Text(title) },
+            supportingContent = { Text(subtitle) },
+            leadingContent = {
+                Icon(Icons.Filled.Person, contentDescription = title)
+            },
+            colors = ListItemDefaults.colors(
+                containerColor = MaterialTheme.colorScheme.background
+            )
+        )
+    }
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -19,9 +41,10 @@ fun PlayersScreen(modifier: Modifier = Modifier, innerPading: PaddingValues) {
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Top
     ) {
-        Text(
-            text = "Jugadores",
-            modifier = modifier
-        )
+        repeat(5) { cantidad ->
+            HorizontalDivider()
+            PlayerListItem("Jugador $cantidad", "Masculino")
+            HorizontalDivider()
+        }
     }
 }
--- app/src/main/java/com/example/tenisappf/screens/TournamentsScreen.kt
+++ app/src/main/java/com/example/tenisappf/screens/TournamentsScreen.kt
@@ -5,6 +5,13 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.SportsTennis
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.ListItemDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -12,6 +19,21 @@ import androidx.compose.ui.Modifier
 
 @Composable
 fun TournamentsScreen(modifier: Modifier = Modifier, innerPading: PaddingValues) {
+    @Composable
+    fun TournamentListItem(title: String, subtitle: String, status: String) {
+        ListItem(
+            headlineContent = { Text(title) },
+            supportingContent = { Text(subtitle) },
+            trailingContent = { Text(status) },
+            leadingContent = {
+                Icon(Icons.Filled.SportsTennis, contentDescription = "Localized description")
+            },
+            colors = ListItemDefaults.colors(
+                containerColor = MaterialTheme.colorScheme.background
+            )
+        )
+    }
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -19,8 +41,10 @@ fun TournamentsScreen(modifier: Modifier = Modifier, innerPading: PaddingValues)
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Top
     ) {
-    Text(
-        text = "Torneos",
-        modifier = modifier
-    )}
+        repeat(5) { cantidad ->
+            HorizontalDivider()
+            TournamentListItem("Torneo $cantidad", "Londres", "En curso")
+            HorizontalDivider()
+        }
+    }
 }
